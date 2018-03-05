a0dd74db06530c473332773f3a9b1d6a3a804792
Glaaki <glaaki@protonmail.com>
2018-03-05 18:51:49 +0100

    reinitialized and create parser as a controller only

--- db/migrate/20180305165645_create_static_pages.rb
+++
@@ -1,8 +0,0 @@
-class CreateStaticPages < ActiveRecord::Migration[5.1]
-  def change
-    create_table :static_pages do |t|
-
-      t.timestamps
-    end
-  end
-end
--- test/controllers/parser_controller_test.rb
+++ test/controllers/parser_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class StaticPageTest < ActiveSupport::TestCase
+class ParserControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
