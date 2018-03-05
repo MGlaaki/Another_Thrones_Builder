62cdd71de25a155ee5853fc90634c4e1dbdc94ca
Glaaki <glaaki@protonmail.com>
2018-03-05 18:00:10 +0100

    index page for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :static_pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#index'
 end
