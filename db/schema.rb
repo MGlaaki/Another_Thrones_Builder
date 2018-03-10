f469aed5d4f3c8b0941c85034611bfa6f69f0c3f
Glaaki <glaaki@protonmail.com>
2018-03-10 13:13:27 +0100

    new db

--- db/schema.rb
+++ db/schema.rb
@@ -27,9 +27,4 @@ ActiveRecord::Schema.define(version: 20180305193734) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "static_pages", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
